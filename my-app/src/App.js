64e49d8ca397b6f9e30a974ab8033ce2cf0b1592
Merekeyev Dias <dikxarper.programming@gmail.com>
2024-04-03 17:11:46 +0500

    added bootstrap library

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import $ from 'jquery'; 
+import Popper from 'popper.js'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <div className="container"></div>
     </div>
   );
 }
